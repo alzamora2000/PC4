c2fb161b31e3388d67007c2bfa95787ce86486b0
alzamora2000 <162034762+alzamora2000@users.noreply.github.com>
2024-04-10 22:01:41 +0000

    solucion3

--- solucion3.py
+++ solucion3.py
@@ -1,11 +1,11 @@
 import zipfile
 import os
 
-def comprimir_imagen(imagen, nombre_zip):
+def comprimirImagen(imagen, nombre_zip):
     with zipfile.ZipFile(nombre_zip, 'w') as archivo_zip:
         archivo_zip.write(imagen)
 
-def descomprimir_zip(nombre_zip, carpeta_destino):
+def descomprimirZip(nombre_zip, carpeta_destino):
     with zipfile.ZipFile(nombre_zip, 'r') as archivo_zip:
         archivo_zip.extractall(carpeta_destino)
 
@@ -13,11 +13,11 @@ ruta_imagen = 'imagen.jpg'
 
 nombre_zip = 'imagen_comprimida.zip'
 
-comprimir_imagen(ruta_imagen, nombre_zip)
+comprimirImagen(ruta_imagen, nombre_zip)
 
 carpeta_destino = 'imagen_descomprimida'
 
 
-descomprimir_zip(nombre_zip, carpeta_destino)
+descomprimirZip(nombre_zip, carpeta_destino)
 
 print("Proceso completado.")
